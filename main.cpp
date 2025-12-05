91fef84cd6fe8348f52019b2c6a6a87547cca708
Audrey Malcuit <abmal06@outlook.com>
2025-12-05 11:17:40 -0500

    test scroll code in credits

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <FEHImages.h>
 #include <FEHUtility.h>
 #include <FEHKeyboard.h>
+#include <FEHRandom.h>
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
+
+#define SCREEN_WIDTH 319
+#define SCREEN_HEIGHT 239
+#define PIXELS_PER_FRAME 3
 
 void drawMenu();
 void drawPlay();
@@ -13,17 +16,15 @@ void drawInstructions();
 void drawCredits();
 void introScreen();
 void nextGameFrame(bool reset);
-void userInput();
 void endScreen();
 
-//Generative AI was used to create images throughout the game - for more thorough accreditation, please see our website
+// Make compiler happy
+class Coin;
+class Car;
+class Bus;
+void generateNewRow(std::vector<Coin> *coins, std::vector<Car> *cars, std::vector<Bus> *buses);
+void deleteOffScreenObjects(std::vector<Coin> *coins, std::vector<Car> *cars, std::vector<Bus> *buses);
 
-/*
-Audrey Malcuit
-Creation of the player class
-Declaration of instance variables and instantiation of coordinates and initial appearance on screen according to input of lane
-This class creates the player and also creates two functions (moveUp and moveDown) for player movement between lanes which will be called in response to user input
-*/
 class Player {
     private:
         int lane;
@@ -55,63 +56,38 @@ class Player {
     }
     //X Position will be top left corner of picture to draw;
     // Would be cool if we can make these smooth
-
     void moveUp()
     {
-        LCD.SetFontColor(BLACK);
-        int old_y = y_pos;
-      
         if(lane == 2)
         {
             lane = 1;
             y_pos = 50;
-
-            // Erase old sprite then draw at new position
-            LCD.FillRectangle(x_pos, old_y, SPRITE_W, SPRITE_H);
-            image.Draw(x_pos, y_pos);
         }
         else if(lane == 3)
         {
             lane = 2;
             y_pos = 98;
-
-            // Erase old sprite then draw at new position
-            LCD.FillRectangle(x_pos, old_y, SPRITE_W, SPRITE_H);
-            image.Draw(x_pos, y_pos);
         } 
-
-        
     }
     void moveDown()
     {
-        LCD.SetFontColor(BLACK);
-        int old_y = y_pos;
-
         if(lane == 2)
         {
             lane = 3;
             y_pos = 146;
-            // Erase old sprite then draw at new position
-            LCD.FillRectangle(x_pos, old_y, SPRITE_W, SPRITE_H);
-            image.Draw(x_pos, y_pos);
         }
         else if(lane == 1)
         {
             lane = 2;
             y_pos = 98;
-            // Erase old sprite then draw at new position
-            LCD.FillRectangle(x_pos, old_y, SPRITE_W, SPRITE_H);
-            image.Draw(x_pos, y_pos);
         } 
+    }
+    // Redraw player each frame
+    void draw() {
+        image.Draw(x_pos, y_pos);
     }
 };
 
-/*
-Luke Butcher
-Creation of Coin class - radius of coin for dimension and plotting purposes, coordinates to draw and lane for logic with player and game in general
-Constructor: depends on the lane input and then will update coordinates accordingly
-nextFrame: function to move the coin across the screen
-*/
 class Coin {
     private:
         int lane = 1; // Default lane
@@ -129,30 +105,24 @@ class Coin {
         else if(lane == 3){
             y_pos = 3 * SCREEN_HEIGHT / 4 - COIN_RADIUS;
         }
-        x_pos = 320; // Start at right of screen
-
+        x_pos = SCREEN_WIDTH + COIN_RADIUS*2; // Start at right of screen
+    }
+    void updatePosition(){
+        // Move coin left across the screen
+        x_pos -= PIXELS_PER_FRAME; // Move left
+    }
+    // Redraw coin each frame
+    void draw(){
         // Until we get graphics, represent coin as a circle
         LCD.SetFontColor(YELLOW);
         LCD.FillCircle(x_pos, y_pos, COIN_RADIUS);
         LCD.SetFontColor(WHITE);
     }
-    void nextFrame(){
-        // Move coin left across the screen
-        LCD.SetFontColor(BLACK);
-        LCD.FillCircle(x_pos, y_pos, COIN_RADIUS); // Erase old position
-        x_pos -= 5; // Move left 5 pixels
-        LCD.SetFontColor(YELLOW);
-        LCD.FillCircle(x_pos, y_pos, COIN_RADIUS); // Draw at new position
-        LCD.SetFontColor(WHITE);
+    int getXPos(){
+        return x_pos;
     }
 };
 
-/*
-Audrey Malcuit
-Creation of scrollImage class to make the setting seem as if it was moving at the top and bottom of the screen
-Boolean to determine if it is at the top or bottom (for coordinate purposes), the actual image, and the coordinates
-nextFrame: function to move the images across the screen
-*/
 class scrollImage
 {
     private:
@@ -161,8 +131,6 @@ class scrollImage
         int x_pos, y_pos;
     public:
     //Will have to create starting images with x as input
-
-    //Empty constructor for array
     scrollImage()
     {
 
@@ -173,41 +141,38 @@ class scrollImage
         {
             image.Open("topSideWalk.png");
             x_pos = x;
-            y_pos = 0; //top y coordinate
+            y_pos = 0;
         }
         else
         {
-            image.Open("bottomSidewalk.png"); //inverted image of top
+            image.Open("bottomSidewalk.png");
             x_pos = x;
-            y_pos = 192; //bottom y coordinate
+            y_pos = 192;
         }
         image.Draw(x_pos, y_pos);
     }
-    void nextFrame()
+    void updatePosition()
     {
-        x_pos = x_pos - 3; //Move left 3 units
-        if(x_pos < 0)
+        x_pos = x_pos - 5;
+        if(x_pos < -50)
         {
-            x_pos = 320; //Don't draw past 0
+            x_pos = 320;
         }
+        
+    }
+    void draw()
+    {
         image.Draw(x_pos, y_pos);
-
-
     }
 };
 
-/*
-Luke Butcher
-Creation of car class: lane, coordinates, and actual image
-Constructor: update coordinates and placement of image according to input of lane
-nextFrame: move the car across the screen
-*/
 class Car {
     private:
         int lane;
         int x_pos, y_pos;
         FEHImage carSprite;
     public:
+        static const int CAR_WIDTH = 45;
     Car(int lane){
         if (lane == 1){
             y_pos = SCREEN_WIDTH / 5;
@@ -216,19 +181,20 @@ class Car {
         } else if (lane == 3){
             y_pos = SCREEN_WIDTH / 5 + 92;
         }
-        x_pos = 320;
+        x_pos = SCREEN_WIDTH;
 
         carSprite.Open("Car.png");
+    }
+    void updatePosition(){
+        // Move car left across the screen
+        x_pos -= PIXELS_PER_FRAME;
+    }
+    // Redraw car each frame
+    void draw(){
         carSprite.Draw(x_pos, y_pos);
     }
-    void nextFrame(){
-        // Move car left across the screen
-        LCD.SetFontColor(BLACK);
-        LCD.FillRectangle(x_pos, y_pos, 60 , 30); // Erase old car
-        LCD.SetFontColor(WHITE);
-
-        x_pos -= 5;
-        carSprite.Draw(x_pos, y_pos);
+    int getXPos(){
+        return x_pos;
     }
 };
 
@@ -246,42 +212,40 @@ class Bus {
         } else if (lane == 3){
             y_pos = SCREEN_WIDTH / 5 + 92;
         }
-        x_pos = 320;
+        x_pos = SCREEN_WIDTH;
 
         busSprite.Open("Bus.png");
+    }
+
+    void updatePosition(){
+        // Move bus left across the screen
+        x_pos -= PIXELS_PER_FRAME;
+    }
+    // Redraw bus each frame
+    void draw(){
         busSprite.Draw(x_pos, y_pos);
     }
-    void nextFrame(){
-        // Move bus left across the screen
-        LCD.SetFontColor(BLACK);
-        LCD.FillRectangle(x_pos, y_pos, 80, 30); // Erase old bus
-        LCD.SetFontColor(WHITE);
-
-        x_pos -= 5;
-        busSprite.Draw(x_pos, y_pos);
+    int getXPos(){
+        return x_pos;
     }
 };
 
 int main()
 {
+    // Don't print warnings
+    freopen("/dev/null", "w", stdout);
+
     drawMenu();
 }
 
-/*
-Audrey Malcuit and Luke Butcher
-Displays the menu screen and allows for buttons to be clicked and reroutes users to those screens
-*/
 void drawMenu() {
-    LCD.Clear(); //Clear screen
-    //Constants for boxes
+    LCD.Clear();
     int boxWidth = SCREEN_WIDTH / 1.5;
     int boxHeight = SCREEN_HEIGHT / 8;
     FEHImage menu;
-    menu.Open("Menu Screen.png"); //Menu background image
+    menu.Open("Menu Screen.png");
     menu.Draw(0,0);
     
-
-    //Create boxes of same dimensions, alter y coordinates throughout
     LCD.SetFontColor(BLACK);
     LCD.DrawRectangle(SCREEN_WIDTH / 2 - boxWidth / 2, 50, boxWidth, boxHeight); // Play Button
     LCD.FillRectangle(SCREEN_WIDTH / 2 - boxWidth / 2, 50, boxWidth, boxHeight);\
@@ -308,7 +272,6 @@ void drawMenu() {
     float x_pos, y_pos, x_dummy, y_dummy;
     bool boxTouched = false;
 
-    //Loop until a button is pressed then respond to input and call respective function
     while (!boxTouched){
         while (!LCD.Touch(&x_pos, &y_pos));
         while (LCD.Touch(&x_dummy, &y_dummy));
@@ -332,10 +295,6 @@ void drawMenu() {
     }
 }
 
-/*
-Audrey Malcuit and Luke Butcher
-Finish this comment later
-*/
 void drawPlay()
 {
     LCD.Clear();
@@ -361,12 +320,12 @@ void drawPlay()
         // Run game frames until back button is pressed
         while (!LCD.Touch(&x_pos, &y_pos)){
             nextGameFrame(reset);
-            Sleep(100); // Frame rate - should be faster as time goes on eventually
+            Sleep(20); // Frame rate - should be faster as time goes on eventually
             reset = false;
         }
         while (LCD.Touch(&x_dummy, &y_dummy)){
             nextGameFrame(reset);
-            Sleep(100); // Frame rate - should be faster as time goes on eventually
+            Sleep(20); // Frame rate - should be faster as time goes on eventually
             reset = false;
         }
         
@@ -378,13 +337,9 @@ void drawPlay()
     drawMenu();
 }
 
-/*
-Audrey Malcuit
-Creation of AI images to display a beginning storyline to set up user for gameplay
-*/
+
 void introScreen()
 {
-    //Cycle through storyline screens
     LCD.Clear();
     FEHImage first;
     first.Open("Evil Money Guy Updated2.png");
@@ -409,59 +364,154 @@ void introScreen()
 
 void nextGameFrame(bool reset){
     // All objects should be declared static so their locations/states are maintained
-    // Should probably implement with object arrays
-    /* This is for TESTING right now, eventually will need to randomly generate coins and cars, check collisions, delete off-screen objects, etc.*/
     // Reminder, the object constructors take in lane number (1 = left, 2 = center, 3 = right)
-    static Coin coin1(1);
-    static Coin coin2(2);
-    static Coin coin3(3);
-
-    static Car car1(1);
-    static Car car2(2);
-    static Car car3(3);
-    
-    static Bus bus1(3);
-    static Bus bus2(3);
-    static Bus bus3(3);
+    static std::vector<Coin> coins;
+    static std::vector<Car> cars;
+    static std::vector<Bus> buses;
 
     static Player player(2);
 
-    // reset game when coming back from menu - this needs updated
+    // Count frames for object generating timing
+    static int frameCount = -1;
+    frameCount++;
+
+    // Reset game when coming back from menu
     if (reset) {
-        coin1 = Coin(1);
-        coin2 = Coin(2);
-        coin3 = Coin(3);
-        car1 = Car(1);
-        car2 = Car(2);
-        bus1 = Bus(3);
+        // Recreate all objects
+        frameCount = 0;
+        coins.clear();
+        cars.clear();
+        buses.clear();
         player = Player(2);
     }
 
-    // Move all objects down/next animation frame
-    coin1.nextFrame();
-    coin2.nextFrame();
-    coin3.nextFrame();
-    car1.nextFrame();
-    car2.nextFrame();
-    car3.nextFrame();
-    bus1.nextFrame();
-    bus2.nextFrame();
-    bus3.nextFrame();
-    
-    // Move when arrow keys pressed
-    if (Keyboard.isPressed(KEY_LEFT) || Keyboard.isPressed(KEY_UP)){
-        player.moveUp();
-    } else if (Keyboard.isPressed(KEY_RIGHT) || Keyboard.isPressed(KEY_DOWN)){
-        player.moveDown();
+    // Handle random generation of obstacles/coins
+    // Every time a row of objects moves a car's width, generate new row
+    // I don't like C :( - why can't I use the dot operator with static variables?
+    if ((frameCount % (Car::CAR_WIDTH / PIXELS_PER_FRAME)) == 0) {
+        generateNewRow(&coins, &cars, &buses);
     }
 
-    // TODO: Delete objects that go off screen and create new ones at top (prob do this in class methods)
+    // Clear screen to get ready for redraw
+    LCD.Clear();
+
+    // Update positions
+    for (int i = 0; i < coins.size(); i++) {
+        coins[i].updatePosition();
+    }
+    for (int i = 0; i < cars.size(); i++) {
+        cars[i].updatePosition();
+    }
+    for (int i = 0; i < buses.size(); i++) {
+        buses[i].updatePosition();
+    }
+    
+    /* Move when arrow keys pressed, but can can't move two frames in a row to
+    prevent player from basically teleporting*/
+    static bool wasUpPressed = false;
+    static bool wasDownPressed = false;
+    
+    bool isUpPressed = Keyboard.isPressed(KEY_LEFT) || Keyboard.isPressed(KEY_UP);
+    bool isDownPressed = Keyboard.isPressed(KEY_RIGHT) || Keyboard.isPressed(KEY_DOWN);
+    
+    // Check is key was pressed last frame as well
+    if (!wasUpPressed && isUpPressed) {
+        player.moveUp();
+    } else if (!wasDownPressed && isDownPressed) {
+        player.moveDown();
+    }
+    
+    // Update previous presses for next frame
+    wasUpPressed = isUpPressed;
+    wasDownPressed = isDownPressed;
+
+    // Clean up objects that go off screen
+    deleteOffScreenObjects(&coins, &cars, &buses);
+
+    // Redraw lanes after clearing
+    LCD.SetFontColor(WHITE);
+    LCD.DrawLine(0, SCREEN_HEIGHT / 5, SCREEN_WIDTH, SCREEN_HEIGHT / 5);
+    LCD.DrawLine(0, SCREEN_HEIGHT * 2 / 5, SCREEN_WIDTH, SCREEN_HEIGHT * 2 / 5);
+    LCD.DrawLine(0, SCREEN_HEIGHT * 3 / 5, SCREEN_WIDTH, SCREEN_HEIGHT * 3 / 5);
+    LCD.DrawLine(0, SCREEN_HEIGHT * 4 / 5, SCREEN_WIDTH, SCREEN_HEIGHT * 4 / 5);
+
+    // Redraw all objectss
+    for (int i = 0; i < coins.size(); i++) {
+        coins[i].draw();
+    }
+    for (int i = 0; i < cars.size(); i++) {
+        cars[i].draw();
+    }
+    for (int i = 0; i < buses.size(); i++) {
+        buses[i].draw();
+    }
+    
+    // Redraw player
+    player.draw();
+
+    // Redraw back button
+    LCD.DrawRectangle(5, 5, 20, 20);
+    LCD.WriteAt("<", 5, 5);
 }
 
-/*
-Audrey Malcuit and Luke Butcher
-Update this once statistic tracking is figured out
-*/
+void generateNewRow(std::vector<Coin> *coins, std::vector<Car> *cars, std::vector<Bus> *buses){
+    // Track if bus was last so we know not to generate a vehicle again right after
+    static int busLast = 0;
+    static int lastCoinLane;
+
+    if (busLast == 0) {
+        // Generate coin in any lane
+        int coinLane = (Random.RandInt())/10923 + 1; // Give random int from one to 3
+        (*coins).push_back(Coin(coinLane));
+        lastCoinLane = coinLane;
+
+        int obstacleLane = (Random.RandInt())/10923 + 1;
+        // Make sure obstacle is not in same lane as coin
+        while (obstacleLane == coinLane){
+            obstacleLane = (Random.RandInt())/10923 + 1;
+        }
+
+        int obstacleType = (Random.RandInt())/16383;
+        if (obstacleType == 0){
+            (*cars).push_back(Car(obstacleLane));
+        }else{
+            (*buses).push_back(Bus(obstacleLane));
+            busLast++;
+        }
+    } else if (busLast == 1) {
+        // Generate coin in the last lane it was in
+        (*coins).push_back(Coin(lastCoinLane));
+        busLast++;
+    } else if (busLast == 2){
+        // Generate coin in any lane
+        int coinLane = (Random.RandInt())/10923 + 1; // Give random int from one to 3
+        (*coins).push_back(Coin(coinLane));
+        lastCoinLane = coinLane;
+
+        // Reset busLast
+        busLast = 0;
+    }
+}
+
+void deleteOffScreenObjects(std::vector<Coin> *coins, std::vector<Car> *cars, std::vector<Bus> *buses){
+    
+    for (int i = (*coins).size() - 1; i >= 0; i--) {
+        if ((*coins)[i].getXPos() < -10){
+            (*coins).erase((*coins).begin() + i);
+        }
+    }
+    for (int i = (*cars).size() - 1; i >= 0; i--) {
+        if ((*cars)[i].getXPos() < -45){
+            (*cars).erase((*cars).begin() + i);
+        }
+    }
+    for (int i = (*buses).size() - 1; i >= 0; i--) {
+        if ((*buses)[i].getXPos() < -90){
+            (*buses).erase((*buses).begin() + i);
+        }
+    }
+}
+
 void drawStatistics()
 {
     LCD.Clear();
@@ -502,16 +552,15 @@ void drawStatistics()
     drawMenu();
 }
 
-/*
-Audrey Malcuit and Luke Butcher
-Displays instructions screen and allows user to navigate back to menu screen
-*/
 void drawInstructions()
 {
     LCD.Clear();
+    LCD.SetFontScale(0.5);
     FEHImage instructions;
-    instructions.Open("fixed_instructions.png"); //Instructions picture
+    instructions.Open("fixed_instructions.png");
     instructions.Draw(0,0);
+
+    
 
     float x_pos, y_pos, x_dummy, y_dummy;
     bool exit = false;
@@ -519,8 +568,6 @@ void drawInstructions()
     //Draw back button
     LCD.DrawRectangle(5, 5, 20, 20);
     LCD.WriteAt("<", 5, 5);
-
-    //Loop until button is pressed then navigate back to menu
     while(!exit)
     {
         while (!LCD.Touch(&x_pos, &y_pos));
@@ -544,19 +591,7 @@ void drawCredits()
     //Fix this - tested intro screens
     
     //LCD.SetFontScale(0.5);
-    //LCD.WriteAt("Made by: Audrey Malcuit and Luke Butcher", 20, 50);
-   
-
-
     
-      
-    LCD.SetFontScale(1.0);
-    
-    //Fix this - tested intro screens
-   
-    //LCD.SetFontScale(0.5);
-   
-    //This is test code for the scrolling image 
     
     scrollImage top[12];
     scrollImage bottom[12];
@@ -570,24 +605,30 @@ void drawCredits()
     }
     while(i < 1000)
     {
-       
-
+        
 
         for(int i = 0; i < 12; i++)
         {
-            top[i].nextFrame();
-            bottom[i].nextFrame();
+            top[i].updatePosition();
+            bottom[i].updatePosition();
+            top[i].draw();
+            bottom[i].draw();
         }
-       
-        LCD.Update();
+
+        Sleep(20);
         i++;
     }
      
 
-    
     FEHImage credits;
     credits.Open("creditScreen.png");
     credits.Draw(0,0);
+    
+
+    
+      
+    LCD.SetFontScale(1.0);
+    
     
  
     LCD.SetFontColor(WHITE);  
@@ -612,10 +653,6 @@ void drawCredits()
 
 
 //parameter of stats
-/*
-Audrey Malcuit
-finish this comment later once stats are figured out
-*/
 void endScreen()
 {
     LCD.Clear();
@@ -650,5 +687,4 @@ void endScreen()
     }
     LCD.Clear();
     drawMenu();
-
 }
