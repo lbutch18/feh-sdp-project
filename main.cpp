8dff696b95e60a3079bd6f64858e9d578f15191e
Luke Butcher <butcher_l@icloud.com>
2025-12-06 10:16:12 -0500

    Intro screen basics

--- main.cpp
+++ main.cpp
@@ -471,7 +471,7 @@ void drawPlay()
     LCD.Clear();
     trackStats.resetStats();
 
-    //TO DO: Intro screens
+    introScreen();
 
     float x_pos, y_pos, x_dummy, y_dummy;
     bool exit = false;
@@ -517,8 +517,18 @@ void introScreen()
     FEHImage first;
     first.Open("Evil Money Guy Updated2.png");
     first.Draw(0,0);
-
     Sleep(2.5);
+    int rad = 0;
+    LCD.SetFontColor(WHITE);
+    while (rad < SCREEN_WIDTH){
+        LCD.FillCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, rad);
+        rad += 2; // arbitrary
+        Sleep(1);
+    }
+    LCD.SetFontColor(BLACK);
+
+
+
     FEHImage second;
     second.Open("Planning Guy Updated.png");
     second.Draw(0,0);
